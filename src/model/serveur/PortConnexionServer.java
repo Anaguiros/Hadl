312ad4de483a100b4fe0644efca30aafdb10f4f4
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-19 17:20:33 +0100

    Add ServeurConfiguration classes

---
+++ src/model/serveur/PortConnexionServer.java
@@ -0,0 +1,11 @@
+package model.serveur;
+
+import metaModel.configuration.PortConfigRequis;
+
+public class PortConnexionServer extends PortConfigRequis {
+
+	public PortConnexionServer(String name) {
+		super(name);
+	}
+
+}
--- src/model/serveur/connectionManager/ConnectionManager.java
+++
@@ -1,9 +0,0 @@
-package model.serveur.connectionManager;
-
-public class ConnectionManager {
-
-	public ConnectionManager() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
