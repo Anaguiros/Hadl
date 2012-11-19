5a9bb2e87cfbee562bb8101cd7e4f7021d55fe5f
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-19 15:32:33 +0100

    Add getters and setters in ServiceSingleRequis class

--- src/metaModel/composant/single/ServiceSingleRequis.java
+++ src/metaModel/composant/single/ServiceSingleRequis.java
@@ -1,10 +1,38 @@
 package metaModel.composant.single;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceSingleRequis extends ServiceSingle {
 
 	//Aggrégation de ports
-	List<PortComposantSingleRequis> listPort;
+	List<PortComposantSingleRequis> listPorts;
 	
+	public ServiceSingleRequis(String name) {
+		this.name = name;
+		this.listPorts = new ArrayList<PortComposantSingleRequis>();
+	}
+	
+	public void addPortComposantSingleFourni(PortComposantSingleRequis port) {
+		if (!this.listPorts.contains(port)) {
+			this.listPorts.add(port);
+		}
+	}
+	
+	public void removePortComposantSingleFourni(PortComposantSingleRequis port) {
+		if (this.listPorts.contains(port)) {
+			this.listPorts.remove(port);
+		}
+	}
+	
+	public PortComposantSingleRequis getPortComposantSingleFourni(String name) {
+		PortComposantSingleRequis returnedPort = null;
+		for (PortComposantSingleRequis port : this.listPorts) {
+			if (port.getName().equals(name)) {
+				returnedPort = port;
+				break;
+			}
+		}
+		return returnedPort;
+	}
 }
