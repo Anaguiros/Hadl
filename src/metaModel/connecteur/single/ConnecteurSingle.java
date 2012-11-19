35494aa0a33eab27713cce5525480e5b76b85dc4
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-19 16:11:07 +0100

    Add getters and setters in ConnecteurComposite and ConnecteurSingle classes

--- src/metaModel/connecteur/single/ConnecteurSingle.java
+++ src/metaModel/connecteur/single/ConnecteurSingle.java
@@ -1,18 +1,50 @@
 package metaModel.connecteur.single;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import metaModel.connecteur.Connecteur;
 
 public class ConnecteurSingle extends Connecteur {
 
 	//Composition d'interfaces
-	List<InterfaceConnecteurSingle> listInterface;
+	private ArrayList<InterfaceConnecteurSingle> listInterfaces;
 
+	public ConnecteurSingle(String name) {
+		this.name = name;
+		this.listInterfaces = new ArrayList<InterfaceConnecteurSingle>();
+	}
+	
+	public void addInterfaceConnecteurSingle(InterfaceConnecteurSingle ics) {
+		if (!this.listInterfaces.contains(ics)) {
+			this.listInterfaces.add(ics);
+		}
+	}
+	
+	public void removeInterfaceConnecteurSingle(String name) {
+		for (InterfaceConnecteurSingle ics : this.listInterfaces) {
+			if (ics.getName().equals(name)) {
+				this.listInterfaces.remove(ics);
+				break;
+			}
+		}
+	}
+	
+	public InterfaceConnecteurSingle getInterfaceConnecteurSingle(String name) {
+		InterfaceConnecteurSingle returnedIcs = null;
+		for (InterfaceConnecteurSingle ics : this.listInterfaces) {
+			if (ics.getName().equals(name)) {
+				returnedIcs = ics;
+				break;
+			}
+		}
+		return returnedIcs;
+	}
+	
 	@Override
 	protected void glue() {
 		// TODO Auto-generated method stub
 		
 	}
+	
 	
 }
