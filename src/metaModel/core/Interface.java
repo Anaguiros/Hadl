7f91a2211bbc4c94d32c6942b3c9fe53c931e0f8
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-19 14:35:45 +0100

    Add Type and Synchronisation in Interface class

--- src/metaModel/core/Interface.java
+++ src/metaModel/core/Interface.java
@@ -2,4 +2,17 @@ package metaModel.core;
 
 public abstract class Interface {
 
+	public enum Type {
+		PRIVATE,
+		PUBLIC
+	}
+	
+	public enum Synchronisation {
+		ASYNCHRONE,
+		SYNCHRONE
+	}
+	
+	protected Type type;
+	protected Synchronisation synchronisation;
+	
 }
