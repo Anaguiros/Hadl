7db17cced47edf3982d9a75061e31c140ee82785
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-19 15:55:49 +0100

    Add constructors for PortComposantSingle requis and fourni

--- src/metaModel/composant/single/PortComposantSingleFourni.java
+++ src/metaModel/composant/single/PortComposantSingleFourni.java
@@ -2,4 +2,8 @@ package metaModel.composant.single;
 
 public class PortComposantSingleFourni extends PortComposantSingle {
 
+	public PortComposantSingleFourni(String name) {
+		this.name = name;
+	}
+	
 }
--- src/metaModel/composant/single/PortComposantSingleRequis.java
+++ src/metaModel/composant/single/PortComposantSingleRequis.java
@@ -2,4 +2,8 @@ package metaModel.composant.single;
 
 public class PortComposantSingleRequis extends PortComposantSingle {
 
+	public PortComposantSingleRequis(String name) {
+		this.name = name;
+	}
+	
 }
