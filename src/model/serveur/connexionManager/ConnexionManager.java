25ea9e316a9d6ecc0e26b2e274cab87215cea413
kevin-simon <kevin.simon@kekekiwi.fr>
2012-11-23 15:32:41 +0100

    Commit before pull...

--- src/model/serveur/connexionManager/ConnexionManager.java
+++ src/model/serveur/connexionManager/ConnexionManager.java
@@ -2,8 +2,54 @@ package model.serveur.connexionManager;
 
 public class ConnexionManager {
 
+	private ServiceConnexion serviceConnexion;
+	private ServiceResultat serviceResultat;
+	private ServiceRequete serviceRequete;
+	private ServiceResultsAuth serviceResultsAuth;
+	private ServiceRequeteAuth serviceRequeteAuth;
+	
 	public ConnexionManager() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceConnexion getServiceConnexion() {
+		return serviceConnexion;
+	}
+
+	public void setServiceConnexion(ServiceConnexion serviceConnexion) {
+		this.serviceConnexion = serviceConnexion;
+	}
+
+	public ServiceRequeteAuth getServiceRequeteAuth() {
+		return serviceRequeteAuth;
+	}
+
+	public void setServiceRequeteAuth(ServiceRequeteAuth serviceRequeteAuth) {
+		this.serviceRequeteAuth = serviceRequeteAuth;
+	}
+
+	public ServiceResultsAuth getServiceResultsAuth() {
+		return serviceResultsAuth;
+	}
+
+	public void setServiceResultsAuth(ServiceResultsAuth serviceResultsAuth) {
+		this.serviceResultsAuth = serviceResultsAuth;
+	}
+
+	public ServiceRequete getServiceRequete() {
+		return serviceRequete;
+	}
+
+	public void setServiceRequete(ServiceRequete serviceRequete) {
+		this.serviceRequete = serviceRequete;
+	}
+
+	public ServiceResultat getServiceResultat() {
+		return serviceResultat;
+	}
+
+	public void setServiceResultat(ServiceResultat serviceResultat) {
+		this.serviceResultat = serviceResultat;
 	}
 
 }
