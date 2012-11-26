2a1f8cefcf8d1013408381b9978ab122e9f4e739
Anaguiros <matthieu.lenogue@gmail.com>
2012-11-26 15:35:12 +0100

    tentative observer sur clearance Request

--- src/model/serveur/attachments/RequeteAuth.java
+++ src/model/serveur/attachments/RequeteAuth.java
@@ -1,15 +1,25 @@
 package model.serveur.attachments;
 
+import java.util.Observable;
+
 import metaModel.composant.composite.PortComposantCompositeFourni;
 import metaModel.connecteur.composite.RoleCompositeRequis;
 import metaModel.core.AttachmentFourni;
+import model.serveur.connector.clearanceRequest.RoleRequeteAuth;
+import model.serveur.connexionManager.PortRequeteAuth;
 
 public class RequeteAuth extends AttachmentFourni {
 
-	public RequeteAuth(PortComposantCompositeFourni portCompo,
-			RoleCompositeRequis role) {
+	public RequeteAuth(PortRequeteAuth portCompo,
+			RoleRequeteAuth role) {
 		super(portCompo, role);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void update(Observable observable, Object object){
+		if(object instanceof RoleRequeteAuth){
+			((RoleRequeteAuth) this.role).receive(object);
+		}
 	}
 
 }
