67f2a4b15ba36e3d921379ed496b465151f64ad4
Anaguiros <matthieu.lenogue@gmail.com>
2012-11-26 15:43:49 +0100

    Chemin ServiceRequeteAuth -> Connector oki normalement

--- src/model/serveur/attachments/RequeteAuth.java
+++ src/model/serveur/attachments/RequeteAuth.java
@@ -1,6 +1,7 @@
 package model.serveur.attachments;
 
 import java.util.Observable;
+import java.util.Observer;
 
 import metaModel.composant.composite.PortComposantCompositeFourni;
 import metaModel.connecteur.composite.RoleCompositeRequis;
@@ -8,8 +9,8 @@ import metaModel.core.AttachmentFourni;
 import model.serveur.connector.clearanceRequest.RoleRequeteAuth;
 import model.serveur.connexionManager.PortRequeteAuth;
 
-public class RequeteAuth extends AttachmentFourni {
-
+public class RequeteAuth extends AttachmentFourni implements Observer{
+	
 	public RequeteAuth(PortRequeteAuth portCompo,
 			RoleRequeteAuth role) {
 		super(portCompo, role);
@@ -17,7 +18,7 @@ public class RequeteAuth extends AttachmentFourni {
 	}
 	
 	public void update(Observable observable, Object object){
-		if(object instanceof RoleRequeteAuth){
+		if(object instanceof PortRequeteAuth){
 			((RoleRequeteAuth) this.role).receive(object);
 		}
 	}
