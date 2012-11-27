f02c1379117ab66a3b8423da558782fe734f68fb
Anaguiros <matthieu.lenogue@gmail.com>
2012-11-27 15:12:58 +0100

    Ajout de GrantedAuthMessage... je l'ai pas oublié cette fois.

---
+++ src/model/core/AuthGrantedMessage.java
@@ -0,0 +1,21 @@
+package model.core;
+
+public class AuthGrantedMessage extends Message {
+
+	private String credentials;
+	
+	public AuthGrantedMessage(String credentials){
+		this.contenu = DEFAULT_CONTENT;
+		this.credentials = credentials;
+	}
+
+	public String getCredentials() {
+		return credentials;
+	}
+
+	@Override
+	public String toString() {
+		return "AuthGrantedMessage [credentials=" + credentials + ", contenu="
+				+ contenu + "]";
+	}
+}
